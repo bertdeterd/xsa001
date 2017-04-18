5a51ebbe3781d0a09c572fcb0f7e521289f223ff
CR0009 <CR0009@unknown>
2017-04-18 21:46:39 +0000

    router

--- js/api/users/users_api.js
+++ js/api/users/users_api.js
@@ -4,33 +4,16 @@ var bodyParser = require("body-parser");
 var xssec = require("sap-xssec");
 var xsHDBConn = require("sap-hdbext");
 var xsenv = require("sap-xsenv");
- 
-module.exports = function(){
-	var app = express();
-	app.use(bodyParser.json());
 
-	app.route("/users")
-	  .get(function(req, res) {
-	    res.send("Hello World Node.js");
-	});
-
-   return app;
-};
-
-/*
-var express = require("express");
 var userRouter = express.Router();
-//var passport = require("passport");
 
 var router = function() {
 
 	userRouter.route('/users')
 		.get(function(req, res) {
-			console.log(req.body);
-
+			 res.send("Hello World Node.js");
 		});
 	return userRouter;
 };
 
 module.exports = router;
-*/
